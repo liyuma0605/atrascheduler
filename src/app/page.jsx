42617a4f0103df7098135b65fa21213b00508cbc
liyuma0605 <leomyrcayao8@gmail.com>
2025-08-15 18:33:11 +0800

    adding week 5

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1048,6 +1048,7 @@ export default function ScheduleEditor() {
         "Expected Shifts/Week 2",
         "Expected Shifts/Week 3",
         "Expected Shifts/Week 4",
+        "Expected Shifts/Week 5",
         "Expected Days/Cutoff 15th",
         "Expected Days/Cutoff 30th",
         "Days Rendered",
@@ -1654,6 +1655,7 @@ export default function ScheduleEditor() {
                           <SelectItem value={2}>Week 2</SelectItem>
                           <SelectItem value={3}>Week 3</SelectItem>
                           <SelectItem value={4}>Week 4</SelectItem>
+                          <SelectItem value={5}>Week 5</SelectItem>
                         </SelectContent>
                       </Select>
                     </div>
